57cd9753c1e5e6c88e588c118b0efa0a0acd4b47
mayingchun <168911762@qq.com>
2023-11-23 21:03:58 +0800

    自定义 userDetailService 添加

---
+++ src/main/java/com/myc/boot/service/UserDetailServiceImpl.java
@@ -0,0 +1,35 @@
+package com.myc.boot.service;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.myc.boot.domain.LoginUserDetails;
+import com.myc.boot.domain.User;
+import com.myc.boot.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // 查询用户信息
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName, username);
+        User user = userMapper.selectOne(queryWrapper);
+        if(user == null){
+            throw new UsernameNotFoundException("用户不存在");
+        }
+
+        // 存在设置用户权限
+
+        return new LoginUserDetails(user);
+    }
+}
